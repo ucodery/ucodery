ab86a717b35749026a463ba39b596cd113611e62
Jeremy Paige <ucodery@gmail.com>
2023-02-07 14:13:52 -0800

    fix extend_readme

--- extend_readme.py
+++ extend_readme.py
@@ -61,43 +61,37 @@ def filter_and_sort_repos(repos):
 
 def build_html(colab_repos):
     html_template = cleandoc('''
-    <picture>
-    <source
-      srcset="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true&theme=dark"
-      media="(prefers-color-scheme: dark)"
-    />
-    <source
-      srcset="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true"
-      media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
-    />
-    <img src="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true" />
-    </picture>
+    <a href="https://github.com/{user}/{repo}">
+      <picture>
+        <source
+          srcset="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true&theme=dark"
+          media="(prefers-color-scheme: dark)"
+        />
+        <source
+          srcset="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true"
+          media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
+        />
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username={user}&repo={repo}&show_owner=true" />
+      </picture>
+    </a>
     ''')
     return '\n'.join(html_template.format(user=user, repo=repo) for user, repo in colab_repos) + '\n'
 
 
 def replace_readme(extra_content):
     with open('README.md', 'r') as readme_read, open('README.new', 'w') as readme_write:
-        print("OPEN")
         for line in readme_read:
             readme_write.write(line)
             if line.strip() == '<!-- replace start -->':
-                print("REPLACING!")
                 readme_write.write(extra_content)
                 while line.strip() != '<!-- replace end -->':
                     line = next(readme_read)
                 readme_write.write(line)
-            else:
-                print(repr(line))
     os.rename('README.new', 'README.md')
-    print(os.path.abspath('README.md'))
 
 
 if __name__ == '__main__':
     repos = make_pulls_query()
-    print(f"{repos=}")
     readme_repos = filter_and_sort_repos(repos)
-    print(f"{readme_repos=}")
     html = build_html(readme_repos)
-    print(f"{html=}")
     replace_readme(html)
