f487db98857bc916223eecc66b7e3147118d906f
Jeremy Paige <ucodery@gmail.com>
2023-02-07 13:11:09 -0800

    write new readme

--- get_colab_repos.py
+++ get_colab_repos.py
@@ -3,6 +3,7 @@ import requests
 
 def make_pulls_query():
     url='https://api.github.com/graphql'
+    auth = {'Authorization': 'Bearer {token}'.format(token=os.environ['TOKEN'])}
     query_template = '''
     {{
       user(login: "ucodery") {{
@@ -14,7 +15,7 @@ def make_pulls_query():
           nodes {{
             repository {{
               stargazerCount
-              url
+              name
               owner {{
                 login
               }}
@@ -24,25 +25,73 @@ def make_pulls_query():
       }}
     }}
     '''
-    auth = {'Authorization': 'Bearer {token}'.format(token=os.environ['TOKEN'])}
     more_pages = True
     after_token = 'null'
     pushed_repos = {}
     while more_pages:
         maybe_query = requests.post(url, json={'query': query_template.format(after=after_token)}, headers=auth)
         maybe_query.raise_for_status()
-        if errors := maybe_query.json().get('errors'):
-                print(errors)
+        if errors := maybe_query.json().get('errors'): print(errors)
         pulls = maybe_query.json()['data']['user']['pullRequests']
         more_pages = pulls['pageInfo']['hasNextPage']
         after_token = pulls['pageInfo']['endCursor']
-        for node in pulls['nodes']:
-            repo = node['repository']
-            if repo['owner']['login'] not in ('ucodery', 'ActiveState'):
-                pushed_repos[repo['url']] = repo['stargazerCount']
+        pushed_repos |= {
+            f"node['repository']['name']/node['repository']['owner']['login']": {
+                'stars': node['repository']['stargazerCount'],
+                'name': node['repository']['name'],
+                'org': node['repository']['owner']['login']
+            }
+            for node in pulls['nodes']
+            if node['repository']['owner']['login'] not in ('ucodery', 'ActiveState')
+        }
     return pushed_repos
-        
+
+
+def filter_and_sort_repos(repos):
+    return [
+        (repos[r]['org'], repos[r]['name'])
+        for r in sorted(
+            repos,
+            key=lambda k: repos[k]['stars'],
+            reverse=True,
+        )
+    ]
+
+
+def build_html(colab_repos):
+    html_template = '''
+    <picture>
+    <source
+      srcset="https://github-readme-stats.vercel.app/api/pin/?username=python&repo=cpython&show_owner=true&theme=dark"
+      media="(prefers-color-scheme: dark)"
+    />
+    <source
+      srcset="https://github-readme-stats.vercel.app/api/pin/?username=python&repo=cpython&show_owner=true"
+      media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
+    />
+    <img src="https://github-readme-stats.vercel.app/api/pin/?username=python&repo=cpython&show_owner=true" />
+    </picture>
+    '''
+    return '\n'.join(html_template.format(user=user, repo=repo) for user, repo in colab_repos)
+
+
+def replace_readme(extra_content):
+    with open('README.md', 'r') as readme_read, open('README.new', 'w') as readme_write:
+        for line in readme_read:
+            readme_write.write(line)
+            if line == '<!-- replace start -->':
+                readme_write.write(extra_content)
+                while line != '<!-- replace end -->':
+                    line = next(readme_read)
+                readme_write.write(line)
+    os.rename('README.new', 'README.md')
+
 
 if __name__ == '__main__':
     repos = make_pulls_query()
-    print(sorted(repos, key=repos.get))
+    print(f"{repos=}")
+    readme_repos = filter_and_sort_repos(repos)
+    print(f"{repos=}")
+    html = build_html(readme_repos)
+    print(f"{repos=}")
+    replace_readme(html)
